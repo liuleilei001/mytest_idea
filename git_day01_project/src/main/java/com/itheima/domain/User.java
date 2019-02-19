36e3c6bbd20df9344d92067586846781cda0a3d7
liuleilei001 <42605240+liuleilei001@users.noreply.github.com>
2019-02-19 16:55:15 +0800

    新建User.java,提交到本地仓库

--- git_day01_project/src/main/java/com/itheima/domain/User.java
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -13,4 +13,35 @@ public class User {
 
     private String test;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
