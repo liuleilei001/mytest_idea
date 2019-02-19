5ff81adae276fedc65d42890f352f7fbc302f4f0
liuleilei001 <42605240+liuleilei001@users.noreply.github.com>
2019-02-19 16:22:04 +0800

    新建User.java,提交到本地仓库

---
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @Description TODO
+ * @Author llei
+ * @Date 2019/2/19 16:14
+ * @Version v1.0.0
+ **/
+public class User {
+    private String name;
+    private String password;
+}
